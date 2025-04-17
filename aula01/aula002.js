0eb8e2f6517cd91098686df88ec5ac842d1f32fe
André Santos <andremissional@gmail.com>
2025-04-17 18:14:57 -0300

    Array na prática

---
+++ aula01/aula002.js
@@ -0,0 +1,15 @@
+const meuArray = [
+    {
+        nome: 'André',
+        idade: 37,
+        casado: true,
+        conjuge: 'Mírian'
+    },
+    {
+        nome: 'ACarloa Felipe',
+        idade: 36,
+        casado: true,
+        conjuge: 'Carol'
+    }
+
+]
