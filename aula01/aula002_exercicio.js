a325e2f46829798d970f1588dac5a271d7c40ab1
André Santos <andremissional@gmail.com>
2025-04-17 20:20:33 -0300

    exercicios

---
+++ aula01/aula002_exercicio.js
@@ -0,0 +1,33 @@
+
+
+const RJ = 350000
+const SP = 600000
+const PB = 259000
+
+if (RJ> 300000){
+    let cidade = "CIDADE GRANDE"
+    console.log(`O RJ é uma: ${cidade}`)
+} 
+
+if (SP> 300000){
+    let cidade = "CIDADE GRANDE"
+    console.log(`SP é uma: ${cidade}`)
+}
+
+if (PB> 300000){
+    let cidade = "CIDADE GRANDE"
+    console.log(`A PB é uma: ${cidade}`)
+
+}    else if (RJ< 300000) {
+     let cidade = "CIDADE PEQUENA"
+     console.log(`o RJ é uma: ${cidade}`)
+}
+    if (SP< 300000){
+    let cidade = "CIDADE PEQUENA"
+    console.log(`SP é uma: ${cidade}`)
+} 
+
+    if (PB< 300000) {
+    let cidade = "CIDADE PEQUENA"
+    console.log(`A PB é uma: ${cidade}`)
+}
---
+++ aula01/aula002_exercicioB.js
@@ -0,0 +1,16 @@
+
+
+const RJ = 350000
+const SP = 600000
+const PB = 259000
+
+let cidade = (RJ,SP,PB)
+
+
+if (cidade> 30000) {
+    let totalcidade = ('CIDADE GRANDE')
+    console.log(`A cidade é uma ${totalcidade}`)
+} else {
+    let totalcidade = ('CIDADE PEQUENA')
+    console.log(`A cidade é uma ${totalcidade}`)
+}
