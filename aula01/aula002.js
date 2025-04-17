d16613d9f20cb2d526c47c26a31bfa5dee5e272b
André Santos <andremissional@gmail.com>
2025-04-17 18:23:00 -0300

    Update aula002.js

--- aula01/aula002.js
+++ aula01/aula002.js
@@ -3,14 +3,21 @@ const usuarios = [
         nome: 'André',
         idade: 37,
         casado: true,
-        conjuge: 'Mírian'
+        conjuge: 'Mírian',
     },
     {
         nome: 'Carlos Felipe',
         idade: 36,
         casado: true,
-        conjuge: 'Carol'
+        conjuge: 'Carol',
+    },
+    {
+        nome: 'Rone',
+        idade: 32,
+        casado: true,
+        conjuge: 'Patricia',
     }
 
 ]
-console.log(usuarios[1])
+
+console.log(usuarios[2])
