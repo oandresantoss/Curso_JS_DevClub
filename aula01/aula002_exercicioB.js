20e81fcb062f44c2dfcfe083a3b3a7bc5176142c
André Santos <andremissional@gmail.com>
2025-04-18 11:30:52 -0300

    Update aula002_exercicioB.js

--- aula01/aula002_exercicioB.js
+++ aula01/aula002_exercicioB.js
@@ -1,15 +1,16 @@
 
 
-const RJ = String(350000)
+const RJ = (350000)
 const SP = 600000
 const PB = 259000
 
-let cidade = (RJ,SP,PB)
+cidade = (RJ,SP,PB)
 
-
-if (cidade> 30000) {
+if (cidade > 30000) {
     let totalcidade = ('CIDADE GRANDE')
-    console.log(`A cidade do ${RJ} é uma ${totalcidade}`)
+    console.log(`A cidade do RJ  é uma ${totalcidade}`)
+    console.log(`A cidade do SP  é uma ${totalcidade}`)
+    console.log(`A cidade do PB  é uma ${totalcidade}`)
 } else {
     let totalcidade = ('CIDADE PEQUENA')
     console.log(`A cidade é uma ${totalcidade}`)
