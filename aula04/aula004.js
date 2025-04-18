3290dd76e124e0b139c3a20d81f4c507acddd976
André Santos <andremissional@gmail.com>
2025-04-18 12:28:49 -0300

    aula004_function
    /* Funções / Function
            -Trecho de código que é excutado
              SOMENTE quando solicitado ( Parente gente boa )
            - Trecho de código que pode ser usado varia vezes
    
        [ ] O que é uma função e como usar
        [ ] Função VOID (vazia)
        [ ] Função co parâmetros
        [ ] Função Return
        [ ] Arrow Function
    */

---
+++ aula04/aula004.js
@@ -0,0 +1,12 @@
+/* Funções / Function
+        -Trecho de código que é excutado 
+          SOMENTE quando solicitado ( Parente gente boa )
+        - Trecho de código que pode ser usado varia vezes
+
+
+    [ ] O que é uma função e como usar
+    [ ] Função VOID (vazia)
+    [ ] Função co parâmetros
+    [ ] Função Return
+    [ ] Arrow Function
+*/
