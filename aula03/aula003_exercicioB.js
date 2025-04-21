a4b86bdf480336fc47527c2a10f46b4e39751319
André Santos <andremissional@gmail.com>
2025-04-21 15:46:28 -0300

    refação_exercicios

--- aula03/aula003_exercicioB.js
+++ aula03/aula003_exercicioB.js
@@ -1,15 +1,19 @@
-
-
-
-
-function cidade(cidadegrande,cidadepequena){ 
-    if (cidadegrande > 30000){
-        return "CIDADE GRANDE"
+const cidades = [
+    { nome: 'RJ', populacao: 350000 },
+    { nome: 'SP', populacao: 600000 },
+    { nome: 'PB', populacao: 259000 }
+  ];
+  
+  function verificarTipoCidade(cidade) {
+    if (cidade.populacao > 300000) {
+      return 'CIDADE GRANDE';
     } else {
-        return "CIDADE PEQUENA"
+      return 'CIDADE PEQUENA';
     }
-
-}
-
-const RJ  = soma(cidade>3000)
-console.log(RJ)
+  }
+  
+  for (let i = 0; i < cidades.length; i++) {
+    const tipo = verificarTipoCidade(cidades[i]);
+    console.log(`A cidade ${cidades[i].nome} é uma: ${tipo}`);
+  }
+  
